7ad168c919bc3471c02c68b9d0d023ae5231adee
kanapon <itoukana6121@gmail.com>
2024-01-23 17:43:48 +0900

    wip

--- app/src/main/java/com/example/myalbum/main/MainViewModel.kt
+++ app/src/main/java/com/example/myalbum/main/MainViewModel.kt
@@ -1,4 +1,9 @@
 package com.example.myalbum.main
 
-class MainViewModel {
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor(){
+    
 }
