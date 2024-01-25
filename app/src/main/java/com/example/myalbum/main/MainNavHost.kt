a5a1f6e9be55d0361f42356e3983afafbc245aef
kanapon <itoukana6121@gmail.com>
2024-01-25 22:39:21 +0900

    wip

--- app/src/main/java/com/example/myalbum/main/MainNavHost.kt
+++ app/src/main/java/com/example/myalbum/main/MainNavHost.kt
@@ -2,23 +2,12 @@ package com.example.myalbum.main
 
 import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.DrawerState
-import androidx.compose.material3.DrawerValue
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.ModalDrawerSheet
-import androidx.compose.material3.ModalNavigationDrawer
-import androidx.compose.material3.NavigationDrawerItem
-import androidx.compose.material3.Surface
-import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.ActivityNavigator
-import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
@@ -26,8 +15,6 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navigation
-import androidx.navigation.safe.args.generator.models.Destination
-import com.example.myalbum.core.data.DrawerParams
 import com.example.myalbum.core.data.MainNavOption
 import com.example.myalbum.feature.edit.EditScreen
 import com.example.myalbum.feature.preview.PreviewScreen
