0ed5a94cf4bf6a6042caa42b8dc67600d68020ff
kanapon <itoukana6121@gmail.com>
2024-01-27 22:18:41 +0900

    wip

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,7 +54,7 @@ android {
 dependencies {
 
     implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.8.2")
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
     implementation("androidx.compose.ui:ui")
@@ -62,7 +62,6 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.navigation:navigation-compose:2.7.6")
-    implementation("androidx.navigation:navigation-safe-args-generator:2.7.6")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -72,7 +71,6 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
     implementation("com.jakewharton.timber:timber:5.0.1")
-    implementation ("com.google.accompanist:accompanist-navigation-animation:0.33.2-alpha")
 
     // hilt
     implementation("com.google.dagger:hilt-android:2.50")
