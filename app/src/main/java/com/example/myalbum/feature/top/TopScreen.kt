d8df4bba926b01eac917c246b444bde40c15a670
kanako6121 <59358870+kanako6121@users.noreply.github.com>
2024-01-31 15:04:16 +0900

    wip

--- app/src/main/java/com/example/myalbum/feature/top/TopScreen.kt
+++ app/src/main/java/com/example/myalbum/feature/top/TopScreen.kt
@@ -5,16 +5,13 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
 import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Divider
 import androidx.compose.material3.DrawerState
 import androidx.compose.material3.DrawerValue
-import androidx.compose.material3.ExtendedFloatingActionButton
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
@@ -26,7 +23,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
@@ -35,10 +31,9 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.myalbum.R
 import com.example.myalbum.core.data.MainNavOption
-import com.example.myalbum.core.data.PhotoRepository
-import kotlinx.coroutines.launch
 
 @Composable
 fun <T : Enum<T>> TopScreen(
@@ -49,8 +44,71 @@ fun <T : Enum<T>> TopScreen(
     defaultPick: T,
     drawerState: DrawerState,
 ) {
-    var currentPick by remember { mutableStateOf(defaultPick) }
-    val coroutineScope = rememberCoroutineScope()
+    val photos = listOf(
+        R.drawable.seigo1,
+        R.drawable.seigo2,
+        R.drawable.seigo3,
+        R.drawable.seigo4,
+        R.drawable.seigo5,
+        R.drawable.seigo9,
+    )
+    TopScreenContent(
+        onNavigationToEditScreen = onNavigationToEditScreen,
+        onNavigationToPreviewScreen = onNavigationToPreviewScreen,
+        photos = photos
+    )
+}
+
+@Composable
+fun TopScreenContent(
+    photos: List<Int>,
+    onNavigationToEditScreen: () -> Unit,
+    onNavigationToPreviewScreen: () -> Unit,
+) {
+    var selectedPhoto by remember { mutableStateOf(-1) }
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            TopBar(
+                onFinishApp = { },
+                onReload = {},
+            )
+        },
+    ) { paddings ->
+        Column(
+            modifier = Modifier.padding(paddings),
+        ) {
+            LazyVerticalStaggeredGrid(
+                columns = StaggeredGridCells.Fixed(2),
+                content = {
+                    items(photos.size) { index ->
+                        Column(
+                            modifier = Modifier
+                                .padding(start = 8.dp, top = 8.dp, end = 8.dp, bottom = 16.dp)
+                                .shadow(elevation = 4.dp)
+                                //.aspectRatio(1.5f, matchHeightConstraintsFirst = false)
+                                .background(Color.White)
+                                .border(
+                                    BorderStroke(width = 0.5.dp, color = Color.Gray)
+                                )
+                                .padding(start = 8.dp, top = 8.dp, end = 8.dp, bottom = 24.dp),
+                        ) {
+                            Image(
+                                painter = painterResource(id = photos.get(index)),
+                                contentDescription = "Photo",
+                                contentScale = ContentScale.Fit
+                            )
+                            Text(text = "運動会")
+                        }
+                    }
+                }
+            )
+        }
+    }
+}
+
+@Composable
+fun TopBar(onFinishApp: () -> Unit, onReload: () -> Unit) {
     ModalNavigationDrawer(
         drawerContent = {
             ModalDrawerSheet(
@@ -62,75 +120,14 @@ fun <T : Enum<T>> TopScreen(
                 Text("Menu", modifier = Modifier.padding(16.dp))
                 Divider()
                 NavigationDrawerItem(
-                    label = { Text(text = menuItems) },
+                    label = { "title" },
                     selected = false,
-                    onClick = { /*TODO*/ }
+                    onClick = { }
                 )
             }
         },
-    ) {
-        Scaffold(
-            floatingActionButton = {
-                ExtendedFloatingActionButton(
-                    text = { Text("追加") },
-                    icon = { Icon(Icons.Filled.Add, contentDescription = "") },
-                    onClick = {
-                        coroutineScope.launch {
-                            drawerState.apply {
-                                if (isClosed) open() else close()
-                            }
-                        }
-                    }
-                )
-            }
-        )
-        { paddingValues ->
-            Surface {
-                Column(modifier = Modifier.padding(paddingValues)) {
-                    val photos = listOf(
-                        R.drawable.seigo1,
-                        R.drawable.seigo2,
-                        R.drawable.seigo3,
-                        R.drawable.seigo4,
-                        R.drawable.seigo5,
-                        R.drawable.seigo9,
-                    )
-                    TopScreenContent(photos = photos)
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun TopScreenContent(photos: List<Int>) {
-    var selectedPhoto by remember { mutableStateOf(-1) }
-
-    LazyVerticalStaggeredGrid(
-        columns = StaggeredGridCells.Fixed(2),
-        content = {
-            items(photos.size) { index ->
-                Column(
-                    modifier = Modifier
-                        .padding(start = 8.dp, top = 8.dp, end = 8.dp, bottom = 16.dp)
-                        .shadow(elevation = 4.dp)
-                        //.aspectRatio(1.5f, matchHeightConstraintsFirst = false)
-                        .background(Color.White)
-                        .border(
-                            BorderStroke(width = 0.5.dp, color = Color.Gray)
-                        )
-                        .padding(start = 8.dp, top = 8.dp, end = 8.dp, bottom = 24.dp),
-                ) {
-                    Image(
-                        painter = painterResource(id = photos.get(index)),
-                        contentDescription = "Photo",
-                        contentScale = ContentScale.Fit
-                    )
-                    Text(text = "運動会")
-                }
-            }
-        }
     )
+    {}
 }
 
 @Preview(showBackground = true)
@@ -138,7 +135,7 @@ fun TopScreenContent(photos: List<Int>) {
 fun ShowPhotoGrid() {
     TopScreen(
         menuItems = "編集する",
-        viewModel = TopViewModel(repository = PhotoRepository()),
+        viewModel = viewModel(),
         onNavigationToEditScreen = {},
         onNavigationToPreviewScreen = {},
         defaultPick = MainNavOption.TopScreen,
