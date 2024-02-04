fced48918b465226bb5c2302e33a5ef8679b9320
kanapon <itoukana6121@gmail.com>
2024-02-04 17:51:37 +0900

    wip

--- app/src/main/java/com/example/myalbum/core/data/PhotoRepository.kt
+++ app/src/main/java/com/example/myalbum/core/data/PhotoRepository.kt
@@ -1,7 +1,14 @@
 package com.example.myalbum.core.data
 
+import android.content.Context
+import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import javax.inject.Inject
 
-class PhotoRepository @Inject constructor() {
-
+class PhotoRepository @Inject constructor(
+    @ApplicationContext private val applicationContext: Context,
+) {
+    private val _masterFlow = MutableStateFlow(0)
+    val masterFlow = _masterFlow.asStateFlow()
 }
