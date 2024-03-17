de6f9a2a3fa33f9f379f9d8538e7c219706b0212
kanapon <itoukana6121@gmail.com>
2024-03-17 22:07:23 +0900

    wip

--- app/src/main/java/com/example/myalbum/feature/top/TopScreen.kt
+++ app/src/main/java/com/example/myalbum/feature/top/TopScreen.kt
@@ -4,17 +4,15 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
 import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -23,11 +21,9 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -107,24 +103,23 @@ fun TopScreenContent(
             )
         }
     }
-}
+    Scaffold(
+        botBar = { AddButton() },
+        floatingActionButton = {
+            FloatingActionButton(onClick = {
+                println("Clicked!")
+            }) {
+                Icon(Icons.Filled.Add, contentDescription = "追加")
+            }
+        }
+    )
 
 @Composable
-fun DrawerMenuItem(
-    icon: ImageVector,
-    label: String
-) {
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .clickable { }
-            .padding(8.dp),
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        Icon(imageVector = icon, contentDescription = null, modifier = Modifier.size(24.dp))
-        Spacer(modifier = Modifier.width(8.dp))
-        Text(text = label)
+fun AddButton() {
+    FloatingActionButton(onClick = { /*do something*/ }) {
+        Icon(Icons.Filled.Add, contentDescription = "追加")
     }
+}
 }
 
 @Preview(showBackground = true)
