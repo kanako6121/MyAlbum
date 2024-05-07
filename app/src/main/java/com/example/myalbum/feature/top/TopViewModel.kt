d1842d692d8e7362d2da565314cb38ae18f26ad6
kanapon <itoukana6121@gmail.com>
2024-05-07 21:49:17 +0900

    wip

--- app/src/main/java/com/example/myalbum/feature/top/TopViewModel.kt
+++ app/src/main/java/com/example/myalbum/feature/top/TopViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.myalbum.feature.top
 
 import android.net.Uri
+import android.provider.ContactsContract
 import android.util.Log
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
@@ -9,8 +10,11 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import com.example.myalbum.main.MainIntent
+import com.example.myalbum.main.MainState
+import kotlinx.coroutines.channels.Channel
 
-class TopViewModel : ViewModel() {
+class TopViewModel(private val photo: ContactsContract.Contacts.Photo) : ViewModel() {
     val pickMedia = registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
         if (uri != null) {
             Log.d("PhotoPicker", "Selected URI: $uri")
@@ -18,3 +22,4 @@ class TopViewModel : ViewModel() {
             Log.d("PhotoPicker", "No media selected")
         }
     }
+}
--- app/src/main/java/com/example/myalbum/main/MainActivity.kt
+++ app/src/main/java/com/example/myalbum/main/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.myalbum.main
 import android.arch.lifecycle.ViewModelProvider
 import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
+import android.support.v4.app.FragmentActivity
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
@@ -10,19 +11,23 @@ import androidx.activity.viewModels
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.myalbum.core.theme.MyAlbumTheme
 import com.example.myalbum.feature.top.TopScreenContent
 import com.example.myalbum.feature.top.TopViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : ComponentActivity() {
+class MainActivity : FragmentActivity() {
     private lateinit var topViewModel: TopViewModel
     private val launcher = registerForActivityResult(ActivityResultContracts.PickVisualMedia()) {
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        topViewModel = ViewModelProviders.of(viewModelFactory {  })
+
         setContent {
             MyAlbumTheme {
                 MainNav()
