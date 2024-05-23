1adea8b7945b97c475ec37d817099c3b87a093cc
kanapon <itoukana6121@gmail.com>
2024-05-23 09:02:56 +0900

    wip

---
+++ app/src/main/java/com/example/myalbum/core/util/ArticleView.kt
@@ -0,0 +1,18 @@
+package com.example.myalbum.core.util
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import org.w3c.dom.Comment
+
+@Composable
+fun ArticleView(
+    comment: Comment
+) {
+
+}
+
+@Preview
+@Composable
+fun ArticleViewPreview() {
+    
+}
