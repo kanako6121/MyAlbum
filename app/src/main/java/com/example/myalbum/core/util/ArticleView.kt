284495574bffeadbcfbaf71b18da543252210514
kanapon <itoukana6121@gmail.com>
2024-05-23 09:10:17 +0900

    wip

--- app/src/main/java/com/example/myalbum/core/util/ArticleView.kt
+++ app/src/main/java/com/example/myalbum/core/util/ArticleView.kt
@@ -1,18 +1,37 @@
 package com.example.myalbum.core.util
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import org.w3c.dom.Comment
 
 @Composable
 fun ArticleView(
     comment: Comment
 ) {
+    Column {
+        AsyncImage(
+            modifier = Modifier
+                .aspectRatio(1f)
+                .padding(16.dp)
+                .fillMaxWidth(),
+            model =  {
 
+            },
+            contentDescription = null,
+        )
+    }
 }
 
 @Preview
 @Composable
 fun ArticleViewPreview() {
-    
+
 }
