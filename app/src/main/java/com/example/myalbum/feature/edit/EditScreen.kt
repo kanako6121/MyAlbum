af97b9a648b0f0e90157de3811d1163a617f28ee
kanapon <itoukana6121@gmail.com>
2024-05-23 23:15:42 +0900

    wip

--- app/src/main/java/com/example/myalbum/feature/edit/EditScreen.kt
+++ app/src/main/java/com/example/myalbum/feature/edit/EditScreen.kt
@@ -1,123 +1,65 @@
 package com.example.myalbum.feature.edit
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.ArrowBack
-import androidx.compose.material.icons.rounded.Send
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.example.myalbum.R
+import coil.compose.AsyncImage
+import coil.request.CachePolicy
+import coil.request.ImageRequest
+import coil.transform.RoundedCornersTransformation
 
 @Composable
 fun EditScreen(
-    onUpPress: () -> Unit,
+    selectUri: String,
+    onClick: () -> Unit,
+    onChange: (String) -> Unit,
 ) {
-    EditContentScreen(
-    )
-}
-
-@Composable
-fun EditContentScreen(
-) {
-    Scaffold(
-        modifier = Modifier.fillMaxSize(),
-        topBar = {
-            EditTopBar(
-                title = "Myアルバム",
-            )
-        }
-    ) { paddings ->
-        Column(
+    var comment by remember { mutableStateOf("") }
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+    ) {
+        AsyncImage(
             modifier = Modifier
-                .fillMaxWidth()
-                .padding(paddings)
-        ) {
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .align(Alignment.CenterHorizontally)
-            )
-            {
-                Image(
-                    painter = painterResource(id = R.drawable.seigo7),
-                    contentDescription = null,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .align(Alignment.TopCenter),
+                .aspectRatio(1f)
+                .padding(16.dp)
+                .fillMaxWidth(),
+            model = ImageRequest.Builder(LocalContext.current)
+                .data(selectUri)
+                .crossfade(true)
+                .diskCachePolicy(CachePolicy.DISABLED)
+                .transformations(
+                    RoundedCornersTransformation(40f)
                 )
-            }
-            Text(
-                text = "思い出を書いてみよう",
-                modifier = Modifier
-                    .padding(8.dp)
-                    .align(Alignment.CenterHorizontally),
-                fontSize = 20.sp,
-            )
-            Text(
-                text = "2024.1.1",
-                modifier = Modifier
-                    .padding(8.dp)
-                    .align(Alignment.CenterHorizontally),
-                fontSize = 20.sp,
-            )
-            OutlinedTextField(
-                modifier = Modifier.fillMaxWidth(),
-                maxLines = 5,
-                value = "",
-                onValueChange = {
-
-                },
-            )
+                .build(),
+            contentDescription = null
+        )
+        TextField(
+            modifier = Modifier
+                .padding(8.dp)
+                .align(Alignment.CenterHorizontally),
+            value = comment,
+            onValueChange = { newComment ->
+                comment = newComment
+                onChange(newComment)
+            },
+            label = { Text(text = "コメントを入力してください") }
+        )
+        Button(onClick = onClick) {
+            Text(text = "投稿する")
         }
     }
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun EditTopBar(
-    title: String,
-) {
-    TopAppBar(
-        title = { Text(text = title) },
-        modifier = Modifier.fillMaxWidth(),
-        actions = {
-            IconButton(onClick = { }) {
-                Icon(
-                    imageVector = Icons.Rounded.Send,
-                    contentDescription = null
-                )
-            }
-        },
-        navigationIcon = {
-            IconButton(onClick = {}) {
-                Icon(
-                    imageVector = Icons.Rounded.ArrowBack,
-                    contentDescription = null
-                )
-            }
-        }
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-private fun PreviewEditScreen() {
-    EditContentScreen ()
 }
