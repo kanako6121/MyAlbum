7fa3520d672054b1b9ab18d37f0c7ca93f9008c9
kanapon <itoukana6121@gmail.com>
2024-10-31 23:14:48 +0900

    wip

--- app/src/main/java/com/example/myalbum/main/MainNav.kt
+++ app/src/main/java/com/example/myalbum/main/MainNav.kt
@@ -25,7 +25,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
