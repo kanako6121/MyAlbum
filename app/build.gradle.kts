415b67f9cad74ffa6cbf3d270349f62535c7382d
kanapon <itoukana6121@gmail.com>
2024-11-05 00:34:13 +0900

    wip

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,7 +54,6 @@ android {
 }
 
 dependencies {
-    implementation("androidx.lifecycle:lifecycle-runtime-compose-android:2.8.7")
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
 
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
