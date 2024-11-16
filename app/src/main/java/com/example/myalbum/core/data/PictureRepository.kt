3eaff82f1802c7f50e4649d4972c2de4233bec7e
mitsuru <mtsrst@gmail.com>
2024-11-16 23:54:39 +0900

    format

--- app/src/main/java/com/example/myalbum/core/data/PictureRepository.kt
+++ app/src/main/java/com/example/myalbum/core/data/PictureRepository.kt
@@ -1,41 +1,48 @@
 package com.example.myalbum.core.data
 
-import android.net.Uri
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.stateIn
-import org.w3c.dom.Comment
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class PictureRepository @Inject constructor(
-    private val preference: AlbumPreference
+  private val preference: AlbumPreference
 ) {
-    private val scope = CoroutineScope(Dispatchers.IO + SupervisorJob())
+  private val scope = CoroutineScope(Dispatchers.IO + SupervisorJob())
 
-    val albums: Flow<List<AlbumData>> = preference.albumMap.map { it.values.toList() }
+  val albums: Flow<List<AlbumData>> = preference.albumMap.map { it.values.toList() }
 
-    fun createAlbum(title: String) {
-    }
+  fun createAlbum(title: String) {
+  }
 
-    fun updateAlbumTitle(albumId: Int,  newTitle: String ) :AlbumData {
-    }
+  fun updateAlbumTitle(
+    albumId: Int,
+    newTitle: String
+  ) {
+  }
 
-    fun addPhotoToAlbum(albumId: Int, picture: PictureData) {
-    }
+  fun addPhotoToAlbum(
+    albumId: Int,
+    pictureData: PictureData
+  ) {
+  }
 
-    fun updatePicture(albumId: Int, pictureData: PictureData) {
-    }
+  fun updatePicture(
+    albumId: Int,
+    pictureData: PictureData
+  ) {
+  }
 
-    fun removePhoto(albumId: Int, id: Int) {
-    }
+  fun removePhoto(
+    albumId: Int,
+    pictureId: Int
+  ) {
+  }
 
-    fun deleateAlbum(albumId: Int) {
-    }
+  fun deleateAlbum(albumId: Int) {
+  }
 }
