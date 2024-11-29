1613a972cebccc84451186514b6adae68f37c47c
mitsuru <mtsrst@gmail.com>
2024-11-30 00:02:53 +0900

    sample flow to stateflow

--- app/src/main/java/com/example/myalbum/feature/main/ui/MainViewModel.kt
+++ app/src/main/java/com/example/myalbum/feature/main/ui/MainViewModel.kt
@@ -1,41 +1,26 @@
 package com.example.myalbum.feature.main.ui
 
-import android.net.Uri
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.myalbum.core.data.AlbumData
 import com.example.myalbum.core.data.AlbumRepository
-import com.example.myalbum.feature.main.data.AlbumMenu
 import com.example.myalbum.feature.main.data.MainUiState
+import com.example.myalbum.feature.main.data.toMainUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.update
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
   private val repository: AlbumRepository,
 ) : ViewModel() {
-  private val _uiState = MutableStateFlow(MainUiState())
-  val uiState: StateFlow<MainUiState> = _uiState.asStateFlow()
-
-  init {
-    viewModelScope.launch {
-      repository.albums.collect { albums ->
-        val albumMenus = albums.map {
-          AlbumMenu(
-            id = it.id,
-            uri = it.pictures.firstOrNull()?.uri ?: Uri.EMPTY,
-            title = it.title,
-          )
-        }
-        _uiState.update { menu ->
-          menu.copy(albumMenu = albumMenus)
-        }
-      }
-    }
-  }
+  val uiState: StateFlow<MainUiState> = repository.albumsFlow.map { albums ->
+    albums.toMainUiState()
+  }.stateIn(
+    scope = viewModelScope,
+    started = SharingStarted.Eagerly,
+    initialValue = MainUiState()
+  )
 }
