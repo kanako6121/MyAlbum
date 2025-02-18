69dd71326f8722d3a6bf87edb58d746a1749ab72
kanako6121 <59358870+kanako6121@users.noreply.github.com>
2025-02-18 20:06:48 +0900

    zoomable (#32)
    Co-authored-by: kamm <>

--- app/src/main/java/com/example/myalbum/feature/edit/ui/EditPictureScreen.kt
+++ app/src/main/java/com/example/myalbum/feature/edit/ui/EditPictureScreen.kt
@@ -1,10 +1,10 @@
 package com.example.myalbum.feature.edit.ui
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
@@ -26,6 +26,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -48,14 +49,12 @@ fun EditPictureScreen(
 ) {
 
   val uiState by viewModel.uiState.collectAsStateWithLifecycle()
-  val maxChar = 12
 
   LaunchedEffect(key1 = albumId, key2 = pictureId) {
     viewModel.setEditPicture(albumId, pictureId)
   }
   EditPictureContent(
     modifier = Modifier.fillMaxSize(),
-    maxChar = maxChar,
     onUpPress = onUpPress,
     onSaveComment = viewModel::savePicture,
     pictureData = uiState.pictureData,
@@ -65,18 +64,20 @@ fun EditPictureScreen(
 @Composable
 fun EditPictureContent(
   modifier: Modifier = Modifier,
-  maxChar: Int,
   onUpPress: () -> Unit,
   onSaveComment: (String) -> Unit,
   pictureData: PictureData?,
 ) {
   var comment by remember(pictureData) { mutableStateOf(pictureData?.comment.orEmpty()) }
+  val zoomState = rememberZoomState()
 
   Scaffold(
-    modifier = Modifier,
+    modifier = modifier,
     topBar = {
       EditTopBar(
-        modifier = Modifier.fillMaxWidth(),
+        modifier = Modifier.fillMaxWidth().alpha(
+          if (zoomState.scale <= 1f) 1f else 0f
+        ),
         title = { Text(text = stringResource(R.string.edit_photo)) },
         navigationIcon = {
           IconButton(onClick = onUpPress) {
@@ -91,7 +92,6 @@ fun EditPictureContent(
         .padding(contentPadding)
         .fillMaxWidth()
     ) {
-      val zoomState = rememberZoomState()
       AsyncImage(
         onSuccess = { state ->
           zoomState.setContentSize(state.painter.intrinsicSize)
@@ -99,37 +99,34 @@ fun EditPictureContent(
         contentScale = ContentScale.Fit,
         modifier = Modifier
           .zoomable(zoomState)
-          .aspectRatio(1f)
           .padding(8.dp)
-          .fillMaxWidth()
-          .fillMaxHeight(),
+          .fillMaxWidth(),
         model = pictureData?.uri,
         contentDescription = null
       )
-      OutlinedTextField(
-        modifier = Modifier
-          .padding(8.dp)
-          .align(Alignment.CenterHorizontally),
-        value = comment,
-        onValueChange = { newComment ->
-          if (newComment.length <= maxChar) {
+      if (zoomState.scale <= 1f) {
+        OutlinedTextField(
+          modifier = Modifier
+            .padding(start = 8.dp, end = 8.dp)
+            .fillMaxWidth()
+            .align(Alignment.CenterHorizontally),
+          value = comment,
+          onValueChange = { newComment ->
             comment = newComment
+          },
+          singleLine = true,
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Button(
+          modifier = Modifier
+            .align(Alignment.CenterHorizontally),
+          onClick = {
+            onSaveComment(comment)
+            onUpPress()
           }
-
-        },
-        placeholder = { Text(text = stringResource(R.string.comment)) },
-        singleLine = true,
-      )
-      Button(
-        modifier = Modifier
-          .padding(16.dp)
-          .align(Alignment.CenterHorizontally),
-        onClick = {
-          onSaveComment(comment)
-          onUpPress()
+        ) {
+          Text(text = stringResource(R.string.post))
         }
-      ) {
-        Text(text = stringResource(R.string.post))
       }
     }
   }
@@ -157,7 +154,6 @@ fun EditTopBar(
 @Composable
 fun ShowEditPictureContent() {
   EditPictureContent(
-    maxChar = 10,
     onUpPress = {},
     onSaveComment = { _ -> },
     pictureData = PictureData(id = 0, uri = "content://media/external/images/media/1".toUri(), comment = ""),
