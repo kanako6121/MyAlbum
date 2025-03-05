e839a40caeda44c057d3f10b1c7f4073aad1afd1
Mitsuru <mtsrst@gmail.com>
2025-03-05 21:58:42 +0900

    update libs for Android Studio Meerkat (#34)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
   id("com.android.application")
   id("org.jetbrains.kotlin.android")
+  id("org.jetbrains.kotlin.plugin.compose")
   id("org.jetbrains.kotlin.plugin.serialization")
   id("com.google.devtools.ksp")
   id("com.google.dagger.hilt.android")
@@ -43,9 +44,6 @@ android {
   buildFeatures {
     compose = true
   }
-  composeOptions {
-    kotlinCompilerExtensionVersion = "1.5.13"
-  }
   packaging {
     resources {
       excludes += "/META-INF/{AL2.0,LGPL2.1}"
@@ -54,42 +52,44 @@ android {
 }
 
 dependencies {
-  coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.2")
+  coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.5")
 
-  implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
+  implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.0")
   implementation("androidx.core:core-ktx:1.15.0")
   implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
-  implementation("androidx.datastore:datastore-preferences:1.1.1")
+  implementation("androidx.datastore:datastore-preferences:1.1.3")
 
-  implementation("androidx.activity:activity-compose:1.9.3")
-  implementation("androidx.navigation:navigation-compose:2.8.3")
+  implementation("androidx.activity:activity-compose:1.10.1")
+  implementation("androidx.navigation:navigation-compose:2.8.8")
 
-  implementation(platform("androidx.compose:compose-bom:2024.10.01"))
+  implementation(platform("androidx.compose:compose-bom:2025.02.00"))
   implementation("androidx.compose.ui:ui")
   implementation("androidx.compose.ui:ui-graphics")
   implementation("androidx.compose.ui:ui-tooling-preview")
   implementation("androidx.compose.material3:material3")
+  implementation("androidx.compose.foundation:foundation")
+  implementation("androidx.compose.runtime:runtime")
 
-  implementation("io.coil-kt:coil-compose:2.6.0")
+  implementation("io.coil-kt:coil-compose:2.7.0")
   implementation("com.jakewharton.timber:timber:5.0.1")
   implementation( "net.engawapg.lib:zoomable:2.0.0-test2")
 
   testImplementation("junit:junit:4.13.2")
   androidTestImplementation("androidx.test.ext:junit:1.2.1")
   androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
-  androidTestImplementation(platform("androidx.compose:compose-bom:2024.10.01"))
+  androidTestImplementation(platform("androidx.compose:compose-bom:2025.02.00"))
   androidTestImplementation("androidx.compose.ui:ui-test-junit4")
   debugImplementation("androidx.compose.ui:ui-tooling")
   debugImplementation("androidx.compose.ui:ui-test-manifest")
 
   // hilt
-  implementation("com.google.dagger:hilt-android:2.51.1")
-  ksp("com.google.dagger:hilt-android-compiler:2.51.1")
+  implementation("com.google.dagger:hilt-android:2.55")
+  ksp("com.google.dagger:hilt-android-compiler:2.55")
   implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
 
-  testImplementation("com.google.dagger:hilt-android-testing:2.51.1")
-  kspTest("com.google.dagger:hilt-android-compiler:2.51.1")
+  testImplementation("com.google.dagger:hilt-android-testing:2.55")
+  kspTest("com.google.dagger:hilt-android-compiler:2.55")
 
-  androidTestImplementation("com.google.dagger:hilt-android-testing:2.51.1")
-  kspAndroidTest("com.google.dagger:hilt-android-compiler:2.51.1")
+  androidTestImplementation("com.google.dagger:hilt-android-testing:2.55")
+  kspAndroidTest("com.google.dagger:hilt-android-compiler:2.55")
 }
